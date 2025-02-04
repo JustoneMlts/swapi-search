cb787b7a9a825ff72354b9ec96e6f58d035baf81
Justin Maltese <163068370+JustoneMlts@users.noreply.github.com>
2025-02-04 18:33:04 +0100

    auth over

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,35 +1,38 @@
 import type React from "react"
 import { useState } from "react"
 import { Eye, EyeOff } from "lucide-react"
-import  Rebel  from "../assets/rebel.svg"
+import Rebel from "../assets/rebel.svg"
 import styles from "./Login.module.css"
 import { authController } from "../controllers/authController"
+import { Spinner } from "../components/Spinner"
 
 export const Login = ({
   handleLogin
-} : {
+}: {
   handleLogin: () => void;
 }) => {
   const [showPassword, setShowPassword] = useState(false)
   const [username, setUsername] = useState("")
   const [password, setPassword] = useState("")
   const [error, setError] = useState("");
-
+  const [isLoading, setIsLoading] = useState(false);
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault()
+    setIsLoading(true);
     try {
       await authController.login(username, password);
-        handleLogin()
+      handleLogin()
     } catch {
-        setError("Incorrect vos identifiants sont");
+      setError("Incorrect vos identifiants sont");
     }
+    setIsLoading(false);
   }
 
   return (
     <div className={styles.loginPage}>
       <div className={styles.loginContainer}>
         <div className={styles.imageContainer}>
-          <img src={Rebel} className={styles.image}/>
+          <img src={Rebel} className={styles.image} />
         </div>
         <div className={styles.formContainer}>
           <h2 className={styles.title}>Login</h2>
@@ -64,13 +67,16 @@ export const Login = ({
               </div>
             </div>
             <div>
-              {error && 
-              <span className={styles.error}>
-                {error}
-              </span>}
+              {error &&
+                <span className={styles.error}>
+                  {error}
+                </span>}
             </div>
+
             <button type="submit" className={styles.loginButton}>
-              Login
+              {
+                isLoading ? <Spinner /> : "Login"
+              }
             </button>
           </form>
         </div>
